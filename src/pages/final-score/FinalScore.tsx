23250596d93fa0e53d99f362438e22c34a173dac
trunghuy17 <nguyentrunghuy017@gmail.com>
2025-09-21 01:56:34 +0700

    Commit layout FinalScore and Leaderboard

--- src/pages/final-score/FinalScore.tsx
+++ src/pages/final-score/FinalScore.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import {
+  Box,
+  Button,
+  FormControl,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router";
 
 function FinalScore() {
+  const navigate = useNavigate();
+
+  function submit(e: React.FormEvent) {
+    e.preventDefault();
+    navigate("/leader-board");
+  }
   return (
-    <div>FinalScore</div>
-  )
+    <>
+      <Box sx={{ maxWidth: 900, mx: "auto", px: 2, mt: 4 }}>
+        <Typography variant="h3" sx={{ my: 4 }}>
+          Final Score: 0
+        </Typography>
+        <form action="" onSubmit={submit}>
+          <Stack spacing={4}>
+            <FormControl fullWidth>
+              <TextField id="firstName" label="First Name" variant="standard" />
+            </FormControl>
+            <FormControl fullWidth>
+              <TextField id="lastName" label="Last Name" variant="standard" />
+            </FormControl>
+            <FormControl fullWidth>
+              <TextField id="email" label="Email Address" variant="standard" />
+            </FormControl>
+            <Box textAlign="right">
+              <Button
+                type="submit"
+                variant="contained"
+                sx={{ cursor: "pointer" }}
+              >
+                SUBMIT
+              </Button>
+            </Box>
+          </Stack>
+        </form>
+      </Box>
+    </>
+  );
 }
 
-export default FinalScore
+export default FinalScore;
--- src/pages/leaderboard/Leaderboard.tsx
+++ src/pages/leaderboard/Leaderboard.tsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import { Box, Button, Typography } from "@mui/material";
+import DescriptionIcon from "@mui/icons-material/Description";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
-function Leaderboard() {
-  return (
-    <div>Leaderboard</div>
-  )
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number
+) {
+  return { name, calories, fat, carbs };
 }
 
-export default Leaderboard
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24),
+  createData("Ice cream sandwich", 237, 9.0, 37),
+  createData("Eclair", 262, 16.0, 24),
+  createData("Cupcake", 305, 3.7, 67),
+  createData("Gingerbread", 356, 16.0, 49),
+];
+
+function LeaderBoard() {
+  return (
+    <>
+      <Box sx={{ maxWidth: 900, mx: "auto", px: 2, mt: 4 }}>
+        <Typography variant="h3" gutterBottom align="center">
+          Leaderboard
+        </Typography>
+        <Box textAlign="right" sx={{ mb: 2 }}>
+          <Button
+            variant="contained"
+            startIcon={<DescriptionIcon />}
+            sx={{ mr: 2 }}
+          >
+            EXPORT CSV
+          </Button>
+          <Button variant="outlined" type="button">
+            Go Home
+          </Button>
+        </Box>
+        <Box>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>First Name</TableCell>
+                  <TableCell>Last Name</TableCell>
+                  <TableCell>Email</TableCell>
+                  <TableCell>Score</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow
+                    key={row.name}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                    <TableCell>{row.calories}</TableCell>
+                    <TableCell>{row.fat}</TableCell>
+                    <TableCell>{row.carbs}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+      </Box>
+    </>
+  );
+}
+
+export default LeaderBoard;
